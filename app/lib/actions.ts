88a72dd9b99cdcf0e788f6e8f6af60c6662704e8
Dmitriy.Cherniy <Dmitriy.Cherniy@elnk.com>
2026-03-10 20:29:34 +0400

    fix

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -40,7 +40,7 @@ export async function createInvoice(prevState: State, formData: any) {
     status: formData.get("status"),
   });
 
-  // If form validation fails, return errors early. Otherwise, continue.
+  // If form validation fails, return errors early. Other. wise, continue.
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
